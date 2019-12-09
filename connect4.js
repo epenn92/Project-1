9a8357b29b62fb500689d31dff1fa0cac04ee12b
Eric Penn <ep00959@georgiasouthern.edu>
2019-12-09 07:44:28 -0500

    final final cleanups

--- connect4.js
+++ connect4.js
@@ -6,7 +6,7 @@ let playerOneText = document.querySelector('.playerOneText');
 let playerTwoText = document.querySelector('.playerTwoText');
 
 let gameTurns = 0;
-// starting page function
+
 landingPageStart = function () {
     headerArea.textContent = "Welcome to CONNNECT 4 DOTS!!";
     let playerVsPlayer = document.createElement('button');
@@ -24,8 +24,7 @@ landingPageStart = function () {
     playerVsComputer.textContent = "Player VS CPU";
     playArea.append(playerVsComputer);
     playerVsComputer.addEventListener('click', function () {
-        // createGameColumn();
-        // createGameElements();
+
     });
 
     let learningMode = document.createElement('button');
@@ -33,8 +32,6 @@ landingPageStart = function () {
     learningMode.textContent = "Learning Mode";
     playArea.append(learningMode);
     playArea.addEventListener('click', function () {
-        // createGameColumn();
-        // createGameElements();
     });
 
     let howToPlay = document.createElement('button');
@@ -71,7 +68,7 @@ gameModal = function () {
     let closeButton = document.createElement('span');
     myGameModalContent.append(closeButton);
     closeButton.classList.add('closeModal');
-    closeButton.textContent = "Close";
+    closeButton.textContent = "Rematch?";
     myGameModal.style.display = "block";
     closeButton.addEventListener('click', function () {
         myGameModal.style.display = "none";
@@ -83,7 +80,7 @@ createGameColumn = function () {
     for (let i = 0; i < 7; i++) {
         let gameColumn = document.createElement('ol');
         gameColumn.className = `columns${i}`;
-        // gameColumn.className = "columns";
+
         gameColumn.addEventListener('click', userSelection);
         playArea.append(gameColumn);
     }
@@ -97,19 +94,6 @@ createGameColumn = function () {
     });
 }
 
-
-
-// let createPlayerTurn = document.createElement('h2');
-// let createPlayerOne = document.createElement('h1');
-// createPlayerOne.classList.add('playerOneIndicator');
-// createPlayerOne.textContent = "Player 1";
-// headerArea.append(createPlayerOne);
-// headerArea.append(createPlayerTurn);
-// let createPlayerTwo = document.createElement('h1');
-// createPlayerTwo.classList.add('playerTwoIndicator');
-// createPlayerTwo.textContent = "Player 2";
-// headerArea.append(createPlayerTwo);
-// whichPlayerTurn(player);
 createGameElements = function () {
     headerArea.textContent = "May the best player win";
     playerOneText.textContent = "P1";
@@ -126,21 +110,8 @@ createGameElements = function () {
     let gameModal = document.createElement('div');
     gameModal.classList.add('gameModal');
 
-
-    // let endGameButton = document.createElement('button');
-    // endGameButton.classList.add('endGameButton');
-    // endGameButton.textContent = "End Game";
-    // endGameButton.addEventListener('click', function () {
-    //     removeGameState();
-    //     landingPageStart();
-    // })
-    // document.querySelector('main').append(endGameButton);
-
-    // how to end the game and return to start page
     removeGameState = function () {
-        // let allSelections = document.querySelectorAll('li');
-        // allSelections.remove();
-        // let allColumns = document.querySelectorAll('ol');
+
         playArea.remove();
 
         $('.playerfunctions').hide();
@@ -149,9 +120,6 @@ createGameElements = function () {
 
     }
 
-    // changeGameElements = function () {
-    //     whichPlayerTurn(player);
-    // }
     blankGameState = function () {
         player = 1;
         gameTurns = 0;
@@ -173,19 +141,19 @@ let playerOneTag = document.querySelector('.firstPlayer');
 let playerTwoTag = document.querySelector('.secondPlayer');
 let player = 1;
 function userSelection() {
-    // console.log(player);
+
     let playGameColumns = document.querySelectorAll('ol');
     let dropArea = this.querySelectorAll('.rows');
     let changeBoard = dropArea[dropArea.length - 1];
     horizontalWinCondition = function () {
         for (let r = 6; r >= 0; r--)
             for (let c = 0; c < 4; c++) {
-                // console.log(playGameColumns);
+
                 if ((playGameColumns[c].childNodes[r].dataset.player) == player &&
                     (playGameColumns[c + 1].childNodes[r].dataset.player) == player &&
                     (playGameColumns[c + 2].childNodes[r].dataset.player) == player &&
                     (playGameColumns[c + 3].childNodes[r].dataset.player) == player) {
-                    // alert(`${player} won`);
+
                     headerArea.textContent = `Player ${player} has WON!`;
                     gameModal();
                 }
@@ -198,21 +166,21 @@ function userSelection() {
                     (playGameColumns[c].childNodes[r - 1].dataset.player) == player &&
                     (playGameColumns[c].childNodes[r - 2].dataset.player) == player &&
                     (playGameColumns[c].childNodes[r - 3].dataset.player) == player) {
-                    // alert(`${player} won`);
+
                     headerArea.textContent = `Player ${player} has WON!`;
                     gameModal();
                 }
             }
     }
     diagonalUpRightWinCondition = function () {
-        // diagonal up right
+
         for (let r = 6; r >= 3; r--)
             for (let c = 0; c < 4; c++) {
                 if ((playGameColumns[c].childNodes[r].dataset.player) == player &&
                     (playGameColumns[c + 1].childNodes[r - 1].dataset.player) == player &&
                     (playGameColumns[c + 2].childNodes[r - 2].dataset.player) == player &&
                     (playGameColumns[c + 3].childNodes[r - 3].dataset.player) == player) {
-                    // alert(`${player} won`);
+
                     headerArea.textContent = `Player ${player} has WON!`;
                     gameModal();
 
@@ -220,14 +188,14 @@ function userSelection() {
             }
     }
     diagonalDownRightWinCondition = function () {
-        // diagonal down right
+
         for (let r = 0; r <= 3; r++)
             for (let c = 0; c < 4; c++) {
                 if ((playGameColumns[c].childNodes[r].dataset.player) == player &&
                     (playGameColumns[c + 1].childNodes[r + 1].dataset.player) == player &&
                     (playGameColumns[c + 2].childNodes[r + 2].dataset.player) == player &&
                     (playGameColumns[c + 3].childNodes[r + 3].dataset.player) == player) {
-                    // alert(`${player} won`);
+
                     headerArea.textContent = `Player ${player} has WON!`;
                     gameModal();
                 }
@@ -259,12 +227,12 @@ function userSelection() {
             changeBoard.dataset.player = player;
             winningGameConditions();
             gameTurns += 1;
-            console.log(gameTurns);
+
             tieGameConditions();
             playerOneText.classList.remove('playerIndicator');
             playerTwoText.classList.add('playerIndicator');
             player += 1;
-            // whichPlayerTurn(player);
+
 
         }
         else if (player === 2) {
@@ -273,12 +241,12 @@ function userSelection() {
             changeBoard.dataset.player = player;
             winningGameConditions();
             gameTurns += 1;
-            console.log(gameTurns);
+
             tieGameConditions();
             playerTwoText.classList.remove('playerIndicator');
             playerOneText.classList.add('playerIndicator');
             player -= 1;
-            // whichPlayerTurn(player);
+
         }
 
 
